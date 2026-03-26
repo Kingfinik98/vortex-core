02e93df6aea168d884180f9f4e035c018e6e1d2a
waheiiiddd-lab <waheiiiddd@gmail.com>
2026-03-27 02:53:22 +0700

    Implement execRoot method for root command execution
    Added execRoot method to execute root commands.

--- app/src/main/java/com/zixine/engine/ZixineTileService.java
+++ app/src/main/java/com/zixine/engine/ZixineTileService.java
@@ -1,18 +1,25 @@
-// Untuk ZixineTileService.java
 package com.zixine.engine;
 import android.service.quicksettings.*;
+import java.io.DataOutputStream;
 
 public class ZixineTileService extends TileService {
     @Override
     public void onClick() {
         Tile t = getQsTile();
         if (t.getState() == Tile.STATE_INACTIVE) {
-            try { Runtime.getRuntime().exec(new String[]{"su", "-c", "pm disable-user --user 0 com.android.vending"}); } catch(Exception e){}
+            execRoot("pm disable com.miui.powerkeeper/.statemachine.PowerStateMachineService; pm disable-user --user 0 com.android.vending");
             t.setState(Tile.STATE_ACTIVE);
         } else {
-            try { Runtime.getRuntime().exec(new String[]{"su", "-c", "pm enable com.android.vending"}); } catch(Exception e){}
+            execRoot("pm enable com.miui.powerkeeper/.statemachine.PowerStateMachineService; pm enable com.android.vending");
             t.setState(Tile.STATE_INACTIVE);
         }
         t.updateTile();
+    }
+    private void execRoot(String c) {
+        try {
+            java.lang.Process p = Runtime.getRuntime().exec("su");
+            DataOutputStream o = new DataOutputStream(p.getOutputStream());
+            o.writeBytes(c + "\nexit\n"); o.flush();
+        } catch (Exception ignored) {}
     }
 }
