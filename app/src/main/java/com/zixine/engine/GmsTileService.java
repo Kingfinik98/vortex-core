89f7887d706be2c62da27a65ebedac81bab68e65
waheiiiddd-lab <waheiiiddd@gmail.com>
2026-03-27 03:56:54 +0700

    Refactor GmsTileService command execution logic

--- app/src/main/java/com/zixine/engine/GmsTileService.java
+++ app/src/main/java/com/zixine/engine/GmsTileService.java
@@ -1,38 +1,26 @@
 package com.zixine.engine;
-
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.widget.Toast;
-import java.io.DataOutputStream;
 
 public class GmsTileService extends TileService {
     @Override
     public void onClick() {
         Tile t = getQsTile();
         boolean active = (t.getState() == Tile.STATE_INACTIVE);
+        // LIST GOOGLE MANUAL
+        String gms = "com.google.android.gms com.android.vending com.google.android.gsf com.google.android.projection.gearhead com.google.android.tts";
         
         if (active) {
-            // SIKAT GMS: Suspend User 0 + Force Stop semua komponen
-            String cmd = "pm suspend --user 0 com.google.android.gms com.android.vending; " +
-                         "am force-stop com.google.android.gms; " +
-                         "am force-stop com.android.vending; " +
-                         "am kill com.google.android.gms;";
-            exec(cmd);
+            exec("pm suspend --user 0 " + gms + "; am force-stop com.google.android.gms;");
             t.setState(Tile.STATE_ACTIVE);
-            Toast.makeText(this, "GMS: EXECUTED! 💀", 0).show();
+            Toast.makeText(this, "GMS: KILLED 💀", 0).show();
         } else {
-            // RESTORE GMS
-            exec("pm unsuspend --user 0 com.google.android.gms com.android.vending;");
+            exec("pm unsuspend --user 0 " + gms + ";");
             t.setState(Tile.STATE_INACTIVE);
-            Toast.makeText(this, "GMS: ALIVE 🌍", 0).show();
+            Toast.makeText(this, "GMS: RESTORED 🌍", 0).show();
         }
         t.updateTile();
     }
-    private void exec(String c) {
-        try {
-            java.lang.Process p = Runtime.getRuntime().exec("su");
-            DataOutputStream o = new DataOutputStream(p.getOutputStream());
-            o.writeBytes(c + "\nexit\n"); o.flush();
-        } catch (Exception e) {}
-    }
+    private void exec(String c) { try { java.lang.Process p = Runtime.getRuntime().exec("su"); java.io.DataOutputStream o = new java.io.DataOutputStream(p.getOutputStream()); o.writeBytes(c + "\nexit\n"); o.flush(); } catch (Exception e) {} }
 }
