819da1732da873a3d234854cecb758415975f12c
waheiiiddd-lab <waheiiiddd@gmail.com>
2026-03-27 04:03:54 +0700

    Refactor ExtremeTileService for improved package handling

--- app/src/main/java/com/zixine/engine/ExtremeTileService.java
+++ app/src/main/java/com/zixine/engine/ExtremeTileService.java
@@ -1,40 +1,30 @@
 package com.zixine.engine;
-
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.widget.Toast;
-import java.io.DataOutputStream;
 
 public class ExtremeTileService extends TileService {
-    private String EXCLUDES = "com.zcqptx.dcwihze|com.termux|android|com.android.systemui|com.miui.home|com.zixine.engine|com.android.settings|com.dts.freefireth|com.dts.freefiremax|com.mobile.legends|com.tencent.ig|com.pubg.imobile|com.miHoYo.GenshinImpact|com.hoYoverse.hkrpg";
+    private String GAMES = "com.dts.freefireth com.dts.freefiremax com.mobile.legends com.tencent.ig com.pubg.imobile com.miHoYo.GenshinImpact com.hoYoverse.hkrpg";
+    private String WHITELIST = "com.zcqptx.dcwihze com.termux android com.android.systemui com.miui.home com.zixine.engine com.android.settings com.miui.securitycenter";
 
     @Override
     public void onClick() {
         Tile t = getQsTile();
         boolean active = (t.getState() == Tile.STATE_INACTIVE);
-
         if (active) {
-            // EXTREME ON: Filter app, Suspend satu-satu secara berurutan agar tidak ada yang terlewat
-            String cmd = "PKGS=$(pm list packages -e | cut -d ':' -f2 | grep -Ev '" + EXCLUDES + "'); " +
-                         "for p in $PKGS; do pm suspend --user 0 $p; done; " +
-                         "am kill-all; " +
-                         "pm disable com.miui.powerkeeper/.statemachine.PowerStateMachineService;";
+            String cmd = "PKGS=$(pm list packages -e | cut -d ':' -f2); for p in $PKGS; do " +
+                         "MATCH=false; for w in " + WHITELIST + " " + GAMES + "; do [ \"$p\" == \"$w\" ] && MATCH=true && break; done; " +
+                         "[ \"$MATCH\" == \"false\" ] && pm suspend --user 0 $p && am force-stop $p; " +
+                         "done; pm disable com.miui.powerkeeper/.statemachine.PowerStateMachineService;";
             exec(cmd);
             t.setState(Tile.STATE_ACTIVE);
-            Toast.makeText(this, "SYSTEM: SEALED 🛡️", 0).show();
+            Toast.makeText(this, "EXTREME: ON 🛡️", 0).show();
         } else {
-            // EXTREME OFF: Bangunkan SEMUA
-            exec("PKGS=$(pm list packages -u | cut -d ':' -f2); for p in $PKGS; do pm unsuspend --user 0 $p; done; pm enable com.miui.powerkeeper/.statemachine.PowerStateMachineService;");
+            exec("PKGS=$(pm list packages -u | cut -d ':' -f2); for p in $PKGS; do pm unsuspend --user 0 $p & done; pm enable com.miui.powerkeeper/.statemachine.PowerStateMachineService;");
             t.setState(Tile.STATE_INACTIVE);
-            Toast.makeText(this, "SYSTEM: NORMAL 🌍", 0).show();
+            Toast.makeText(this, "EXTREME: OFF 🌍", 0).show();
         }
         t.updateTile();
     }
-    private void exec(String c) {
-        try {
-            java.lang.Process p = Runtime.getRuntime().exec("su");
-            DataOutputStream o = new DataOutputStream(p.getOutputStream());
-            o.writeBytes(c + "\nexit\n"); o.flush();
-        } catch (Exception e) {}
-    }
+    private void exec(String c) { try { java.lang.Process p = Runtime.getRuntime().exec("su"); java.io.DataOutputStream o = new java.io.DataOutputStream(p.getOutputStream()); o.writeBytes(c + "\nexit\n"); o.flush(); } catch (Exception e) {} }
 }
